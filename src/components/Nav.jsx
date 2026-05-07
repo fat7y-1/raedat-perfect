7390a8161ab78d802d3869dd6d824228938e79dc
fathy soliman <fathi123soliman@gmail.com>
2026-05-07 14:08:17 +0300

    f

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,9 +4,8 @@ const Nav = () => {
   return (
     <div>
       <nav className="navbar">
-        
         <div className="nav-links">
-          <Link to="/home">Home</Link>
+          <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/activities">Activities</Link>
           <Link to="/community">Community</Link>
