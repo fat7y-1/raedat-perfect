bfa9436abbe3f81066dc216073acdc3b01566da4
Rehab <rehabmohd03@gmail.com>
2026-05-07 13:45:18 +0300

    udate home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,40 @@
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Say Hello to ra’edat</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <p>
+        ra'edat is a pioneering platform dedicated to unleashing the
+        transformative potential of Arab women within the orange economy.
+        Located in the Kingdom of Bahrain, we serve as a vibrant digital
+        ecosystem where women engage in creative industries, gain essential
+        skills, and connect with a community passionate about artistic and
+        cultural entrepreneurship.
+      </p>
+       <img
+              className="home-img"
+              src="https://www.raedat.online/MediaManager/Media/aboutUs/about_us_sayhello.jpg"
+              alt="home img"
+            />
+
+
+              <img
+              className="home-log"
+              src="https://www.raedat.online/MediaManager/Media/assest/thirdRaedatIcon.svg"
+              alt="home log"
+            />
+
+              <img
+              className="home-log"
+              src="https://www.raedat.online/MediaManager/Media/assest/icon-primaryColor.svg"
+              alt="home log"
+            />
+
+
+
     </div>
   )
 }
